a6ace4392db051e4dd6cb7c9d2f84b0ea37ee97c
Mike Price <mihail.preis@gmail.com>
2021-09-16 19:35:31 +0500

    Addict refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
-mod utils;
-
-use teloxide::prelude::*;
-
 extern crate dotenv;
 
-use crate::utils::result_utils::FatalValueMapper;
+use std::env;
+
 use dotenv::dotenv;
 use lazy_static::lazy_static;
-use std::env;
+use teloxide::prelude::*;
 use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup};
 use teloxide::RequestError;
 use tokio_stream::wrappers::UnboundedReceiverStream;
+
+use crate::utils::result_utils::FatalValueMapper;
+
+mod utils;
 
 static CHANNEL_ID_KEY: &str = "CHANNEL_ID";
 static ADMINS_CHAT_ID_KEY: &str = "ADMINS_CHAT_ID";
 static TELOXIDE_TOKEN_KEY: &str = "TELOXIDE_TOKEN";
 
-fn get_env_key(key: &str) -> String {
-    let value = std::env::var(key);
+static ACCEPT_CALLBACK: &str = "accept";
+static DECLINE_CALLBACK: &str = "decline";
+static WITHOUT_TEXT_CALLBACK: &str = "accept-without-text";
 
-    value.map_value_or_exit(format!("Can not get value for key {}, exiting", key))
+fn get_env_key(key: &str) -> String {
+    env::var(key).map_value_or_exit(format!("Can not get value for key {}, exiting", key))
 }
 
 lazy_static! {
@@ -30,11 +33,10 @@ lazy_static! {
 
 #[tokio::main]
 async fn main() {
-    teloxide::enable_logging!();
     dotenv().ok();
-    let bot = Bot::new(TELOXIDE_TOKEN.to_string());
+    teloxide::enable_logging!();
     log::info!("Bot is running.");
-    Dispatcher::new(bot)
+    Dispatcher::new(Bot::new(TELOXIDE_TOKEN.to_string()))
         .messages_handler(|rx: DispatcherHandlerRx<Bot, Message>| {
             UnboundedReceiverStream::new(rx).for_each_concurrent(None, |cx| async move {
                 match message_handler(cx).await {
@@ -57,13 +59,13 @@ async fn main() {
 
 async fn message_handler(cx: UpdateWithCx<Bot, Message>) -> Result<(), RequestError> {
     let accept_button =
-        InlineKeyboardButton::callback("✅ Accept".to_string(), "accept".to_string());
+        InlineKeyboardButton::callback("✅ Accept".to_string(), ACCEPT_CALLBACK.to_string());
     let accept_without_text_button = InlineKeyboardButton::callback(
         "☢️ Without text".to_string(),
-        "accept-without-text".to_string(),
+        WITHOUT_TEXT_CALLBACK.to_string(),
     );
     let decline_button =
-        InlineKeyboardButton::callback("❌ Decline".to_string(), "decline".to_string());
+        InlineKeyboardButton::callback("❌ Decline".to_string(), DECLINE_CALLBACK.to_string());
     let keyboard = InlineKeyboardMarkup::default()
         .append_row(vec![accept_button, accept_without_text_button])
         .append_row(vec![decline_button]);
@@ -89,7 +91,7 @@ async fn message_handler(cx: UpdateWithCx<Bot, Message>) -> Result<(), RequestEr
 async fn callback_handler(cx: UpdateWithCx<Bot, CallbackQuery>) -> Result<(), RequestError> {
     let data = cx.update.data.clone().ok_or(RequestError::RetryAfter(0))?;
     let message = cx.update.message.ok_or(RequestError::RetryAfter(0))?;
-    if data.starts_with("accept") {
+    if data.starts_with(ACCEPT_CALLBACK) {
         let id = message
             .reply_to_message()
             .ok_or(RequestError::RetryAfter(0))?
@@ -99,7 +101,7 @@ async fn callback_handler(cx: UpdateWithCx<Bot, CallbackQuery>) -> Result<(), Re
             .copy_message(CHANNEL_ID.to_string(), message.chat_id(), id)
             .send()
             .await?;
-        if data.starts_with("accept-without-text") {
+        if data.starts_with(WITHOUT_TEXT_CALLBACK) {
             cx.requester
                 .edit_message_text(CHANNEL_ID.to_string(), _mes.id, "")
                 .send()
--- src/utils/result_utils.rs
+++ src/utils/result_utils.rs
@@ -6,7 +6,7 @@ pub trait FatalValueMapper<T> {
 
 impl<T, E> FatalValueMapper<T> for Result<T, E> {
     fn map_value_or_exit(self, message: String) -> T {
-        self.unwrap_or_else(|e| {
+        self.unwrap_or_else(|_e| {
             error!("{}", message);
             std::process::exit(1)
         })
